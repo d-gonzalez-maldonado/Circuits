7e28db9d92ea4161816f85c68665f1b2e055f082
David GM <dagm@uchicago.edu>
2021-10-14 15:24:53 -0500

    Fixed landing page

---
+++ Quander/precache-manifest.32042af17be94c342cb0e28bbf4b04c5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dedffaaa8d9726cab011ef5b2ce7bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd09f4cadbe815f15012",
+    "url": "./static/css/main.790ee16e.chunk.css"
+  },
+  {
+    "revision": "ba1e7ef9395c32ea255c",
+    "url": "./static/js/2.b3051d4f.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "./static/js/2.b3051d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd09f4cadbe815f15012",
+    "url": "./static/js/main.6fb6aba6.chunk.js"
+  },
+  {
+    "revision": "ed8d604952b1339c9242",
+    "url": "./static/js/runtime-main.a025dc7d.js"
+  },
+  {
+    "revision": "698fbec46a008c927f728dfd5101806d",
+    "url": "./static/media/0.698fbec4.png"
+  },
+  {
+    "revision": "ad1fca36a882ee50b768e17b5531ad48",
+    "url": "./static/media/1.ad1fca36.png"
+  },
+  {
+    "revision": "91ef1e6e084c27ee21d82b795b6ab6c0",
+    "url": "./static/media/2.91ef1e6e.png"
+  },
+  {
+    "revision": "91ef1e6e084c27ee21d82b795b6ab6c0",
+    "url": "./static/media/3.91ef1e6e.png"
+  },
+  {
+    "revision": "f7a6a38aede3432de27ac6818cd759a7",
+    "url": "./static/media/CircuitsGP.f7a6a38a.png"
+  },
+  {
+    "revision": "9c0455389a56e66b8d20eefe3589be04",
+    "url": "./static/media/QueuebitsGP.9c045538.png"
+  },
+  {
+    "revision": "327e0d8962d253867bbdd6ef1ede16ec",
+    "url": "./static/media/QupcakesGP.327e0d89.png"
+  },
+  {
+    "revision": "85bdecea5e5407dbc2a2c19624f6a29b",
+    "url": "./static/media/QupcakesLogo.85bdecea.png"
+  }
+]);
--- Quander/service-worker.js
+++ Quander/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.129e148266ae79b297a2806270299f89.js"
+  "./precache-manifest.32042af17be94c342cb0e28bbf4b04c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
